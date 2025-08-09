2da5d2d5cd1d8693b913f2deac09282d0f160c6e
Akshay Naktode <akshay.naktode14@gmail.com>
2025-08-10 01:29:47 +0530

    EvenOdd Program

---
+++ PracticePrograms/src/InterviewPrograms/EvenOddNumber.java
@@ -0,0 +1,14 @@
+package InterviewPrograms;
+
+public class EvenOddNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 20;
+if(a%2==0)
+	System.out.println(a +" Is a Even Number");
+else
+	System.out.println(a +" Is a Odd Number");
+	}
+
+}
